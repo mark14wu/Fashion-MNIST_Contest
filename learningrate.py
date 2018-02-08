9166558f260496a4770144996857faa4342e71e9
Mark <Mark@wumatoMacBook-Air.local>
2018-02-08 14:57:04 -0500

    50 epochs testing

--- learningrate.py
+++ learningrate.py
@@ -1,8 +1,11 @@
 def learning_rate(lr, epoch):
-    if epoch < 3:
-        factor = 1
-    elif epoch < 4:
-        factor = 10
-    else:
-        factor = 20
-    return lr / factor
+    # if epoch < 3:
+    #     factor = 1
+    # elif epoch < 4:
+    #     factor = 10
+    # else epoch = 5:
+    #     factor = 20
+    # else:
+    #     factor = 100
+    # return lr / factor
+    return lr
