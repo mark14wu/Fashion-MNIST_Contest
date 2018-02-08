3c3ccfd60128e0dc00863bc23348b2e302327d7f
Mark <Mark@wumatoMacBook-Air.local>
2018-02-08 18:20:31 -0500

    dataparallel in ResNet

--- model.py
+++ model.py
@@ -136,10 +136,19 @@ class ResNet(nn.Module):
         self.bn1 = nn.BatchNorm2d(64)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
         self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer1 = nn.DataParallel(self.layer1)
+
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer2 = nn.DataParallel(self.layer2)
+
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer3 = nn.DataParallel(self.layer3)
+
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.layer4 = nn.DataParallel(self.layer4)
+        
         self.avgpool = nn.AvgPool2d(7)
         self.fc = nn.Linear(256 * block.expansion, num_classes)
 
