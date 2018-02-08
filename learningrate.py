4f99f8fd682497b04c659d5ac1fc51d5593eca7e
Mark <Mark@wumatoMacBook-Air.local>
2018-02-07 21:43:15 -0500

    bug fixed

--- learningrate.py
+++ learningrate.py
@@ -1,10 +1,10 @@
 def learning_rate(lr, epoch):
     if epoch < 3:
         factor = 1
-    elif epoch < 6:
+    elif epoch < 33:
         factor = 10
-    elif epoch < 9:
-        factor = 100
+    # elif epoch < 9:
+    #     factor = 100
     else:
-        factor = 1000
+        factor = 100
     return lr / factor
