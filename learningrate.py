45d2e6e354aa45448c60d8fc651a8a13d61eaaf4
Mark <Mark@wumatoMacBook-Air.local>
2018-02-07 21:35:55 -0500

    learning rate modification(divided by 10)

--- learningrate.py
+++ learningrate.py
@@ -2,9 +2,9 @@ def learning_rate(lr, epoch):
     if epoch < 3:
         factor = 1
     elif epoch < 6:
-        factor = 2
+        factor = 10
     elif epoch < 9:
-        factor = 3
+        factor = 100
     else:
-        factor = 5
+        factor = 1000
     return lr / factor
