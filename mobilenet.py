68ec287cb39a88425851688eb0397134c5e04699
Mark <Mark@wumatoMacBook-Air.local>
2018-02-09 08:00:24 -0500

    adjusted learning rate

--- mobilenet.py
+++ mobilenet.py
@@ -69,7 +69,7 @@ output = Dropout(0.5)(base_model.output)
 predict = Dense(10, activation='softmax')(output)
 
 model = Model(inputs=input_image, outputs=predict)
-my_adam = keras.optimizers.Adam(lr=0.00005, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
+my_adam = keras.optimizers.Adam(lr=0.00001, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 model = multi_gpu_model(model, gpus=8)
 model.compile(optimizer=my_adam, loss='sparse_categorical_crossentropy', metrics=['accuracy',f1])
 model.summary()
@@ -120,5 +120,6 @@ def data_generator(X,Y,batch_size=100):
 model.fit(X_train, y_train, batch_size=128, epochs=1000, \
 callbacks=[tensorboard_callback, my_earlystop], shuffle=True, validation_data=(X_test, y_test))
 
-# save model weights
-model.save_weights('mobilenet-saved-models/mobilenet.' + str(int(time.time())) + '.hdf5')
+# save model
+# model.save_weights('mobilenet-saved-models/mobilenet.' + str(int(time.time())) + '.hdf5')
+model.save('mobilenet-saved-models/mobilenet.' + str(int(time.time())) + '.hdf5')
