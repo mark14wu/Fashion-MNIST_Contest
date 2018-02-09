0f7b3ec6449fee1b8cb1aee8a25b0d1680e251d3
Mark <Mark@wumatoMacBook-Air.local>
2018-02-08 22:11:13 -0500

    mobile net visualization

--- mobilenet.py
+++ mobilenet.py
@@ -1,9 +1,9 @@
-import keras
 import numpy as np
 import mnist_reader
 from tqdm import tqdm
 from scipy import misc
 import tensorflow as tf
+
 
 np.random.seed(2017)
 tf.set_random_seed(2017)
@@ -13,25 +13,57 @@ X_test, y_test = mnist_reader.load_mnist('./data', kind='t10k')
 
 height,width = 128,128
 
+
 from keras.applications.mobilenet import MobileNet
 from keras.layers import Input,Dense,Dropout,Lambda
 from keras.models import Model
 from keras import backend as K
+import keras
+
+
+def f1(y_true, y_pred):
+    def recall(y_true, y_pred):
+        """Recall metric.
+
+        Only computes a batch-wise average of recall.
+
+        Computes the recall, a metric for multi-label classification of
+        how many relevant items are selected.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+        recall = true_positives / (possible_positives + K.epsilon())
+        return recall
+
+    def precision(y_true, y_pred):
+        """Precision metric.
+
+        Only computes a batch-wise average of precision.
+
+        Computes the precision, a metric for multi-label classification of
+        how many selected items are relevant.
+        """
+        true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+        predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+        precision = true_positives / (predicted_positives + K.epsilon())
+        return precision
+    precision = precision(y_true, y_pred)
+    recall = recall(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall))
+
+
 
 input_image = Input(shape=(height,width))
 input_image_ = Lambda(lambda x: K.repeat_elements(K.expand_dims(x,3),3,3))(input_image)
-print(input_image_)
 base_model = MobileNet(input_tensor=input_image_, include_top=False, pooling='avg',input_shape=(128,128,3))
 output = Dropout(0.5)(base_model.output)
 predict = Dense(10, activation='softmax')(output)
 
 model = Model(inputs=input_image, outputs=predict)
-
-# Multi-GPU parallelism
-model = keras.utils.multi_gpu_model(model, gpus=8)
-
-model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy',f1])
 model.summary()
+keras.callbacks.TensorBoard(log_dir='./mobilenet_logs', histogram_freq=5, batch_size=100, write_graph=True, write_grads=False, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
+
 
 X_train = X_train.reshape((-1,28,28))
 X_train = np.array([misc.imresize(x, (height,width)).astype(float) for x in tqdm(iter(X_train))])/255.
@@ -61,4 +93,8 @@ def data_generator(X,Y,batch_size=100):
 			yield np.array(p),np.array(q)
 			p,q = [],[]
 
+
+
 model.fit_generator(data_generator(X_train,y_train), steps_per_epoch=600, epochs=50, validation_data=data_generator(X_test,y_test), validation_steps=100)
+
+
