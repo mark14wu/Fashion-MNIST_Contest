b6df0d5beabfef2af02039d634c671fa79b992cd
Mark <Mark@wumatoMacBook-Air.local>
2018-02-08 19:37:17 -0500

    disabled learning rate adjustment

--- optimizer.py
+++ optimizer.py
@@ -2,19 +2,21 @@ import torch
 import torch.optim as optim
 def my_optimizer(parameters, epoch, lr):
     lr = learning_rate(lr, epoch)
-    if epoch <= 4:
-        optimizer = torch.optim.Adam(parameters, lr)
-    else:
-        optimizer = torch.optim.SGD(parameters, lr * 50)
+    # if epoch <= 4:
+    #     optimizer = torch.optim.Adam(parameters, lr=lr)
+    # else:
+    #     optimizer = torch.optim.SGD(parameters, lr=lr * 50)
+    optimizer = torch.optim.Adam(parameters, lr=lr)
     return optimizer
 
 def learning_rate(lr, epoch):
-    if epoch < 4:
-        factor = 1
-    elif epoch < 8:
-        factor = 5
-    elif epoch < 12:
-        factor = 20
-    else:
-        factor = 50
-    return lr / factor
+    # if epoch < 4:
+    #     factor = 1
+    # elif epoch < 8:
+    #     factor = 5
+    # elif epoch < 12:
+    #     factor = 20
+    # else:
+    #     factor = 50
+    # return lr / factor
+    return lr
